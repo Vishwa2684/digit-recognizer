0f3a3f54677ace106cabf198e52f1d911190a2fa
Vishwa <vishwateja2684@gmail.com>
2024-09-27 10:50:08 +0530

    added icon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,10 @@ import React,{useState} from 'react';
 import './App.css';
 import Canvas from './Canvas';
 import Truth from './Truth';
-import { Stack } from '@mui/material';
-// import {LinearProgressWithLabel} from '@mui/material'
+import { Stack, Typography } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+
 function App() {
   const [results,setResults] = useState([]);
   console.log(results)
@@ -14,6 +16,13 @@ function App() {
         <Canvas setResults = {setResults}/>
         <Truth predictions={results}/>
       </Stack>
+      {/* <GitHubIcon onClick ={()=>window.open("https://github.com/Vishwa2684","_blank")}/> */}
+      
+       <Typography>please do check out my GitHub!</Typography>
+        <GitHubIcon
+          onClick={()=>window.open("https://github.com/Vishwa2684","_blank")}
+          style={{ cursor: 'pointer', fontSize: '40px' }}
+        />
       
     </div>
   );
